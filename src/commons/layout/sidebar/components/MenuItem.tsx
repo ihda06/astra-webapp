420d599bdd416c8926b1fe9f8a3f1a33675fcc4c
ihda06 <ihdaanwari5@gmail.com>
2023-10-13 09:46:44 +0700

    refactor: close after click menu

--- src/commons/layout/sidebar/components/MenuItem.tsx
+++ src/commons/layout/sidebar/components/MenuItem.tsx
@@ -3,14 +3,16 @@
 import Link from "next/link";
 import { itemMenu } from "../../../type/menu";
 import { usePathname } from "next/navigation";
+import { useMenu } from "@/context/menu";
 
 export default function MenuItem({ icon, title, desc, path }: itemMenu) {
     const currentPath = usePathname()
     const url = new URL(path, "http://localhost:3000/");
     const active = currentPath === url.pathname ? "bg-indigo-200" : ""
+    const hideMenu = useMenu((state)=>state.hideMenu)
   return (
-    <Link href={path}>
-      <div className={"w-full py-3 px-5 hover:bg-indigo-200 duration-300 rounded-lg cursor-pointer "+active}>
+    <Link href={path} >
+      <div className={"w-full py-3 px-5 hover:bg-indigo-200 duration-300 rounded-lg cursor-pointer "+active} onClick={hideMenu}>
         <div className="flex items-center gap-3">
           <div className=" text-4xl">{icon}</div>
           <div className="inline-block">
