31c030c9cffd7213f2f8a8b1f7afdd9c7034f0b4
ihda-satudental <ihda.anwari@satudental.com>
2024-06-30 12:01:41 +0700

    disable dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,13 +15,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors: {
-        dark: "#121212",
-        light: "#fafafa",
-      },
+      // colors: {
+      //   dark: "#121212",
+      //   light: "#fafafa",
+      // },
     },
   },
   plugins: [],
-  darkMode: "class",
+  darkMode: "selector",
 };
 export default config;
